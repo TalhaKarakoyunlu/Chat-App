6dc80371f23ff065687d477f1eeb934b78e651f8
RecepBorekci <recepborekci29@gmail.com>
2023-06-05 23:10:50 +0300

    Create contact widget. And modify contact page to have a simple look.

--- lib/pages/contacts_page.dart
+++ lib/pages/contacts_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../widgets/contact_widget.dart';
 
 class ContactsPage extends StatelessWidget {
   const ContactsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("ContactsPage"),
+    return Column(
+      children: [
+        TextButton(
+            onPressed: () {
+              //TODO: Implement adding new contact.
+            },
+            child: Text('Add a new contact.')),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 30,
+              itemBuilder: (context, int index) {
+              return ContactWidget(contactCustomName: 'Recep Börekci', contactPhoneNumber: '05552223344', imageURL: "https://talhakarakoyunlu.github.io/cv/images/talha%20profile%20picture.png",);
+          }),
+        ),
+      ],
     );
   }
 }
+
---
+++ lib/widgets/contact_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'avatar.dart';
+
+const contactsTextStyle = TextStyle(
+    color: Colors.black54,
+    fontWeight: FontWeight.w600,
+    fontSize: 20
+);
+
+class ContactWidget extends StatelessWidget {
+  const ContactWidget({
+    Key? key,
+    required this.imageURL,
+    required this.contactCustomName,
+    required this.contactPhoneNumber
+  }) : super(key: key);
+
+  final String imageURL;
+  final String contactCustomName;
+  final String contactPhoneNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      padding: EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Avatar.small(
+            url:
+            imageURL,
+          ),
+          Column(
+            children: [
+              Text(contactCustomName, style: contactsTextStyle,),
+              Text(contactPhoneNumber, style: contactsTextStyle,)
+            ],
+          ),
+          IconButton(
+              onPressed: () {
+                //TODO: Implement adding a totally different user to contacts.
+              },
+              icon: Icon(Icons.call, size: 20, color: Colors.black54,)),
+        ],
+      ),
+    );
+  }
+}
